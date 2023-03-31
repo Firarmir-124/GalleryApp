05fec2599488a516c1d84b7a081cf805d0b7bd8e
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-31 20:13:13 +0600

    #frontend написал контейнер логина

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import {Alert} from "@mui/material";
 
 function App() {
   return (
     <Routes>
       <Route path='register' element={<Register/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='/*' element={<Alert severity='info'>Такой страницы нет !</Alert>}/>
     </Routes>
   );
 }
