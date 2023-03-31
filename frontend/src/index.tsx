34def7a248276aacf81e24a56e412eb8cea84bd9
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-31 20:07:00 +0600

    #frontend дополнил axiosApi новой функцией

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,9 @@ import {PersistGate} from "redux-persist/integration/react";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {GOOGLE_CLIENT_ID} from "./constans";
 import {persistor, store} from "./app/store";
+import {addInterceptors} from "./axiosApi";
+
+addInterceptors(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
