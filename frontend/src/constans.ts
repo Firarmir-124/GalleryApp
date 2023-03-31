981f5d51ec17e55b65555d51120479758c89da94
Polushin Dima <dima2004polushin2004@gmail.com>
2023-03-31 20:22:15 +0600

    #frontend изменил ключ

--- frontend/src/constans.ts
+++ frontend/src/constans.ts
@@ -1,2 +1,2 @@
 export const apiURl = 'http://localhost:8000';
-export const GOOGLE_CLIENT_ID = '452988179716-akr5hke0ca7747roactiuec4ajv8ridf.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '352013163921-ieifu7n1evs8iu48nh6vhi8hpj7ffs9o.apps.googleusercontent.com';
