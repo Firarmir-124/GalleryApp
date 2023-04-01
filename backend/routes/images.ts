f987ab2a4dee0bcc9581984da69eee78e4fe1443
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-01 11:46:45 +0600

    #backend написал логику создания картинок

--- backend/routes/images.ts
+++ backend/routes/images.ts
@@ -1,31 +1,46 @@
 import express from "express";
 import {Image} from "../models/Image";
-import authAnonymous from "../middleware/authAnonymous";
 import {User} from "../models/User";
+import auth from "../middleware/auth";
+import mongoose from "mongoose";
+import {imagesUpload} from "../multer";
 
 const imagesRouter = express.Router();
 
-imagesRouter.get('/', authAnonymous, async (req, res) => {
-  const token = req.get('Authorization');
-
+imagesRouter.get('/', async (req, res) => {
   try {
     if (req.query.imagesUser !== undefined) {
-      const user = await User.findOne({token});
-
-      if (user) {
-        const images = await Image.find({user: user._id});
-        return res.send(images);
-      }
-
+      const images = await Image.find({user: req.query.imagesUser});
+      return res.send(images);
     } else {
-
       const images = await Image.find();
       return res.send(images);
-
     }
 
   } catch (e) {
     return res.sendStatus(500);
+  }
+});
+
+imagesRouter.post('/', auth, imagesUpload.single('image'), async (req, res, next) => {
+  const token = req.get('Authorization');
+
+  try {
+    const user = await User.findOne({token});
+
+    const createImage = await Image.create({
+      user: user && user._id,
+      title: req.body.title,
+      image: req.file && req.file.filename,
+    });
+
+    return res.send(createImage);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    } else {
+      return next(e);
+    }
   }
 });
 
