a7c52b0fdb73f9bcab9db7d89a006d130c9ae53a
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-01 12:09:49 +0600

    #frontend написал логику создания картинок

--- frontend/src/store/imageThunk.ts
+++ frontend/src/store/imageThunk.ts
@@ -1,11 +1,36 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axiosApi from "../axiosApi";
-import {ImagesType} from "../types";
+import {ImagesMutation, ImagesType, ValidationError} from "../types";
+import {isAxiosError} from "axios";
 
 export const getImages = createAsyncThunk<ImagesType[], string>(
   'image/get_images',
   async (query) => {
     const response = await axiosApi.get<ImagesType[]>(query ? '/images?imagesUser=' + query : '/images');
     return response.data;
+  }
+);
+
+export const createImage = createAsyncThunk<void, ImagesMutation, {rejectValue: ValidationError}>(
+  'image/create_image',
+  async (imagesMutation, {rejectWithValue}) => {
+    const formData = new FormData();
+    const keys = Object.keys(imagesMutation) as (keyof ImagesMutation)[];
+
+    keys.forEach((id) => {
+      const value = imagesMutation[id];
+
+      if (value !== null) {
+        formData.append(id, value);
+      }
+    });
+
+    try {
+      await axiosApi.post<ImagesMutation>('/images', formData);
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as ValidationError);
+      }
+    }
   }
 );
