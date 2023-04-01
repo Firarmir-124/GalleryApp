4dad09d77c1189d54d559167c1ac13d5151da001
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-01 16:05:00 +0600

    #backend переделал fixtures

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -17,7 +17,7 @@ const run = async () => {
     console.log('Collections were not present, skipping drop...');
   }
 
-  const userAdmin = await User.create(
+  const [user1, user2, user3] = await User.create(
     {
       email: `admin@mail.com`,
       displayName: 'admin',
@@ -25,35 +25,74 @@ const run = async () => {
       token: crypto.randomUUID(),
       role: 'admin',
       avatar: 'fixtures/avatar.jpg',
+    },
+    {
+      email: `dima@mail.com`,
+      displayName: 'dima',
+      password: '123',
+      token: crypto.randomUUID(),
+      role: 'user',
+      avatar: 'fixtures/avatar.jpg',
+    },
+    {
+      email: `roma@mail.com`,
+      displayName: 'roma',
+      password: '123',
+      token: crypto.randomUUID(),
+      role: 'user',
+      avatar: 'fixtures/avatar.jpg',
     }
   );
 
-  await Image.create({
-    user: userAdmin._id,
-    title: `Картинка админа`,
-    image: `fixtures/image1.jpg`,
-  });
+  await Image.create(
+    {
+      user: user1._id,
+      title: `Картинка админа 1`,
+      image: `fixtures/image1.jpg`,
+    },
+    {
+      user: user1._id,
+      title: `Картинка админа 2`,
+      image: `fixtures/image2.jpg`,
+    },
+    {
+      user: user1._id,
+      title: `Картинка админа 3`,
+      image: `fixtures/image3.jpg`,
+    },
 
-  for (let i = 1; i < 3; i++) {
-    const user = await User.create(
-      {
-        email: `user${i}@mail.com`,
-        displayName: `user${i}`,
-        password: '123',
-        token: crypto.randomUUID(),
-        role: 'user',
-        avatar: 'fixtures/avatar.jpg',
-      }
-    );
+    {
+      user: user2._id,
+      title: `Картинка Димы 1`,
+      image: `fixtures/image1.jpg`,
+    },
+    {
+      user: user2._id,
+      title: `Картинка Димы 2`,
+      image: `fixtures/image2.jpg`,
+    },
+    {
+      user: user2._id,
+      title: `Картинка Димы 3`,
+      image: `fixtures/image3.jpg`,
+    },
 
-    for (let j = 1; j < 5; j++) {
-      await Image.create({
-        user: user._id,
-        title: `Картинка${j}`,
-        image: `fixtures/image${j}.jpg`,
-      });
-    }
-  }
+    {
+      user: user3._id,
+      title: `Картинка Ромы 1`,
+      image: `fixtures/image1.jpg`,
+    },
+    {
+      user: user3._id,
+      title: `Картинка Ромы 2`,
+      image: `fixtures/image2.jpg`,
+    },
+    {
+      user: user3._id,
+      title: `Картинка Ромы 3`,
+      image: `fixtures/image3.jpg`,
+    },
+  );
 
   await db.close();
 };
